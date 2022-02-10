4d465e456d19e641c87e737256b94c8a9e84cfa3
RakeshChotkan <47205952+RakeshChotkan@users.noreply.github.com>
2022-02-10 23:39:49 +0100

    Update nad_commands.py

--- nad_receiver/nad_commands.py
+++ nad_receiver/nad_commands.py
@@ -29,6 +29,10 @@ CMDS: Dict[str, Dict[str, Dict[str, Union[str, Iterable[str]]]]] = {
                 {'cmd': 'Main.LCD.FrontView.Other',
                  'supported_operators': ['+', '-', '=', '?']
                  },
+            'main_lcd_brightness':
+                {'cmd': 'Main.Brightness',
+                 'supported_operators': ['+', '-', '=', '?']
+                 },
             'mute':
                 {'cmd': 'Main.Mute',
                  'supported_operators': ['+', '-', '=', '?']
